61ef9dc0575de7619995812cd0f1740955ab7167
samaksh1203 <samakshwedding2026@gmail.com>
2026-02-01 16:45:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wed/',   
+  base: '/wed/', // ✅ ensures all assets load from /wed/
+  build: {
+    outDir: 'dist'
+  }
 })
