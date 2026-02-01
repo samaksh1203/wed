c3b79dd6603aa50fadb69aeee440b4d3ff17a775
samaksh1203 <samakshwedding2026@gmail.com>
2026-02-01 15:57:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,       // listen on all interfaces (0.0.0.0)
-    port: 8080,
-    strictPort: false // set true to fail if 8080 is busy
-  }
+  base: '/wed/',
 })
